f8c3e8ba5a3f8215302249dd94999c085a6a79c9
Brandon Chinn <brandon@leapyear.io>
2020-01-28 18:57:10 -0800

    Add test for report serialization

---
+++ test/Report.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Report where
+
+import Data.Aeson (encode)
+import qualified Data.IntMap as IntMap
+import Test.Tasty (TestTree)
+import Test.Tasty.Golden (goldenVsString)
+
+import Trace.Hpc.Codecov.Report
+
+test_report_serialization :: TestTree
+test_report_serialization =
+  goldenVsString "report serialization" "test/golden/report_serialization.golden" $ pure $ encode $
+    CodecovReport
+      [ FileReport "src/MyModule/Foo.hs" $ IntMap.fromList
+          [ (1, Hit 0)
+          , (2, Hit 1)
+          , (3, Partial 1 2)
+          , (4, Partial 1 3)
+          , (7, Hit 5)
+          ]
+      , FileReport "src/MyModule/Bar.hs" $ IntMap.fromList
+          [ (10, Hit 50)
+          , (20, Partial 2 5)
+          ]
+      ]
